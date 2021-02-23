f7fe8259ab8240bf4ae477cca266f5d049885eeb
fp024 <aopenasus@naver.com>
2021-02-23 15:26:58 +0900

    Java 8 in Action 연습 프로젝트 생성

---
+++ src/test/java/org/fp024/j8ia/AppTest.java
@@ -0,0 +1,13 @@
+package org.fp024.j8ia;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+	@Test
+	void testApp() {
+		new App().run();
+		assertTrue(true);
+	}
+}
