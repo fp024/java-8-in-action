173c425cd2e449079e50d1f6b5c57e1fdcd6ec66
fp024 <aopenasus@naver.com>
2021-02-25 08:02:57 +0900

    ~ p79 2.4.1 Comparator로 정렬하기

---
+++ src/test/java/org/fp024/j8ia/part01/chapter02/ComparatorSortTest.java
@@ -0,0 +1,55 @@
+package org.fp024.j8ia.part01.chapter02;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 2.4.1 Comparator로 정렬하기
+ */
+@Slf4j
+class ComparatorSortTest {
+	private final List<Apple> appleList = Collections.unmodifiableList(Arrays.asList(
+			Apple.builder().color("green").weight(100).build(), Apple.builder().color("red").weight(160).build(),
+			Apple.builder().color("green").weight(150).build(), Apple.builder().color("yellow").weight(80).build(),
+			Apple.builder().color("white").weight(180).build(), Apple.builder().color("red").weight(140).build()));
+
+	@Test
+	void testComparator() {
+		logger.info("===== 정렬 전 (Java 8 이전)=====");
+		List<Apple> inventory1 = new ArrayList<>(appleList);
+		inventory1.forEach(apple -> logger.info(apple.toString()));
+
+		// 익명 클래스 구현
+		inventory1.sort(new Comparator<Apple>() {
+			@Override
+			public int compare(Apple a1, Apple a2) {
+				return a1.getWeight().compareTo(a2.getWeight());
+			}
+		});
+
+		logger.info("===== 정렬 후 (Java 8 이전)=====");
+		inventory1.forEach(apple -> logger.info(apple.toString()));
+
+		logger.info("===== 정렬 전 (Java 8 에서는...)=====");
+		List<Apple> inventory2 = new ArrayList<>(appleList);
+		inventory2.forEach(apple -> logger.info(apple.toString()));
+
+		// Java 8 람다 표현식
+		inventory2.sort((Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight()));
+
+		logger.info("===== 정렬 후 (Java 8 에서는...)=====");
+		inventory2.forEach(apple -> logger.info(apple.toString()));
+
+		assertEquals(inventory1, inventory2);
+
+	}
+}
