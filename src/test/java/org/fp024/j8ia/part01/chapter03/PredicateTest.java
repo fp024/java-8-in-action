f757e4966619088d16b0dc6651a30b5ca6039108
fp024 <aopenasus@naver.com>
2021-02-26 15:36:25 +0900

    ~ p96 3.4.1 Predicate
    Java에서 미리제공하는 Predicate 인터페이스 테스트

---
+++ src/test/java/org/fp024/j8ia/part01/chapter03/PredicateTest.java
@@ -0,0 +1,48 @@
+package org.fp024.j8ia.part01.chapter03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.fp024.j8ia.part01.chapter02.Apple;
+import org.junit.jupiter.api.Test;
+
+/**
+ * java.util.function.Predicate<T> 를 활용
+ * 
+ * 저자님도 초반에 Predicate를 직접 구현해보도록 가이드 했었었다. Java 8에서 미리 제공하는 Predicate에서도 test
+ * 메서드 부분은 동일하고, default 메서드가 몇개 더 있다.
+ */
+class PredicateTest {
+	private final List<Apple> appleList = Arrays.asList(Apple.builder().color("green").weight(100).build(),
+			Apple.builder().color("red").weight(160).build(), Apple.builder().color("green").weight(150).build(),
+			Apple.builder().color("yellow").weight(80).build(), Apple.builder().color("white").weight(180).build(),
+			Apple.builder().color("red").weight(140).build());
+
+	static <T> List<T> filter(List<T> list, Predicate<T> p) {
+		List<T> result = new ArrayList<>();
+		for (T t : list) {
+			if (p.test(t)) {
+				result.add(t);
+			}
+		}
+		return result;
+	}
+
+	@Test
+	void testPredicate() {
+		List<Apple> result = filter(appleList, (Apple a) -> "red".equals(a.getColor()) && a.getWeight() < 150);
+		assertFalse(result.isEmpty());
+
+		result.forEach(a -> {
+			assertEquals("red", a.getColor());
+			assertTrue(a.getWeight() < 150);
+		});
+	}
+
+}
