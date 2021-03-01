68d36fdeb8368f5caa553af2e51d9ab8820bb171
fp024 <aopenasus@naver.com>
2021-03-01 18:12:43 +0900

    ~ p151 5.1 필터링과 슬라이싱

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 ## Java 8 in Action
-
+* 저자: Raoul-Gabriel Urma, Mario Fusco, Alan Mycroft
 * yes24 도서 판매 링크 (2021년 현시점 품절 상태)
-	* www.yes24.com/Product/Goods/17252419
+    * www.yes24.com/Product/Goods/17252419
 
 * 내용
-Java 8 in Action의 내용을 연습합니다.<br>
-코드의 실행은 Junit5 로 동작시키고, 출력은 Log4j2로 출력합니다.
-<br><br>
-Part 및 Chapter 별로 Java 패키지를 나눕니다.<br>
-프로젝트 분리를 할 필요는 없을 것 같은데, 필요시 분리하도록 합니다.
+    Java 8 in Action의 내용을 연습합니다.<br>
+    코드의 실행은 Junit5 로 동작시키고, 출력은 Log4j2로 출력합니다.<br>
+    Part 및 Chapter 별로 Java 패키지를 나눕니다.<br>
+    프로젝트 분리를 할 필요는 없을 것 같은데, 필요시 분리하도록 합니다.
---
+++ src/test/java/org/fp024/j8ia/part02/chapter05/FilteringAndSlicingTest.java
@@ -0,0 +1,92 @@
+package org.fp024.j8ia.part02.chapter05;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.fp024.j8ia.part02.chapter04.Dish;
+import org.fp024.j8ia.part02.chapter04.Dish.Type;
+import org.fp024.j8ia.part02.chapter04.DishRepository;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 5.1 필터링과 슬라이싱
+ * 
+ */
+class FilteringAndSlicingTest {
+	private List<Dish> menu = DishRepository.getDishList();
+	
+	/**
+	 * 5.1.1 프레디케이트로 필터링
+	 */
+	@Test
+	void testFilteringWithAPredicate() {
+		List<Dish> vegetarianMenu = menu.stream()
+			.filter(Dish::isVegetarian)
+			.collect(toList());
+		
+		vegetarianMenu.forEach(d -> assertTrue(d.isVegetarian()));
+	}
+	
+	
+	/**
+	 * 5.1.2 고유 요소 필터링
+	 */
+	@Test
+	void testFilteringUniqueElementsInAStream() {
+		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+		List<Integer> result = numbers.stream()
+			.filter(i -> i%2==0)
+			.distinct()
+			.collect(toList());
+		
+		assertIterableEquals(Arrays.asList(2, 4), result);
+	}
+	
+	/**
+	 * 5.1.3 스트림 축소
+	 */
+	@Test
+	void testTruncatingAStream() {
+		List<Dish> heavyCalrories = menu.stream()
+			.filter(d -> d.getCalories() > 300)
+			.limit(3)
+			.collect(toList());
+		assertTrue(heavyCalrories.size() <= 3); 
+		heavyCalrories.forEach(d -> assertTrue(d.getCalories() > 300));
+	}
+
+
+	/**
+	 * 5.1.4 요소 건너뛰기
+	 */
+	@Test
+	void testSkippingElements() {
+		List<Dish> dishes = menu.stream()
+				.filter(d -> d.getCalories() > 300)
+				.skip(2)
+				.collect(toList());
+
+		assertIterableEquals(Arrays.asList("chicken", "french fries", "rice", "pizza", "salmon")
+				, dishes.stream().map(Dish::getName).collect(toList()));
+	}
+	
+	
+	/**
+	 * 퀴즈 5.1
+	 * 		처음 등장하는 고기 2개 필터링
+	 */
+	@Test
+	void testQuizFiltering() {
+		List<Dish> result = menu.stream()
+			.filter(d -> d.getType() == Type.MEAT)
+			.limit(2)
+			.collect(toList());
+
+		assertIterableEquals(Arrays.asList("pork", "beef")
+				, result.stream().map(Dish::getName).collect(toList()));
+	}
+}
