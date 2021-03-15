7fbd7cddcbb542cbbe9eaca439f1b4ec919d1072
fp024 <aopenasus@naver.com>
2021-03-15 23:33:23 +0900

    fail()을 잘못사용
    테스트 메서드 이름을 fail()로 지어서, fail()을 무한 재귀호출하는 형태가 되어버림 ㅠㅠ, 관련해서 수정.

--- src/test/java/org/fp024/j8ia/part03/chapter08/RefactoringObjectOrientedDesignPatternsWithLambdasTest.java
+++ src/test/java/org/fp024/j8ia/part03/chapter08/RefactoringObjectOrientedDesignPatternsWithLambdasTest.java
@@ -3,6 +3,7 @@ package org.fp024.j8ia.part03.chapter08;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
@@ -148,7 +149,7 @@ class RefactoringObjectOrientedDesignPatternsWithLambdasTest {
 	}
 	
 	@Test
-	void fail() {
+	void testFail() {
 		fail();
 	}
 }
